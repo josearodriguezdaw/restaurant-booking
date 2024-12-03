450ce714447f4671ed1039450356c8b10367c2b0
Jose Antonio <profesorjoseantoniorodriguez@gmail.com>
2024-12-03 20:57:19 +0100

    Añadida estructura del proyecto

---
+++ src/app/components/booking/booking-resume/booking-resume.component.ts
@@ -0,0 +1,14 @@
+import { Component, Input } from '@angular/core';
+import { Booking, BookingStatus } from '../../../models/booking.model';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-booking-resume',
+  standalone: true,
+  imports: [],
+  templateUrl: './booking-resume.component.html',
+  styleUrl: './booking-resume.component.css'
+})
+export class BookingResumeComponent {
+
+}
