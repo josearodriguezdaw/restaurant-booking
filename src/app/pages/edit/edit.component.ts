51b44127f340c5d99f53acdf0103ce3881069a2e
Jose Antonio <profesorjoseantoniorodriguez@gmail.com>
2024-12-09 12:18:34 +0100

    Ej8: display form edit

--- src/app/pages/edit/edit.component.ts
+++ src/app/pages/edit/edit.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FooterComponent } from '../../components/footer/footer.component';
 import { HeaderComponent } from '../../components/header/header.component';
 import { CommonModule } from '@angular/common';
+import { BookingFormComponent } from '../../components/booking/booking-form/booking-form.component';
 
 @Component({
   selector: 'app-edit',
   standalone: true,
-  imports: [CommonModule, HeaderComponent,FooterComponent],
+  imports: [CommonModule, HeaderComponent,FooterComponent,BookingFormComponent],
   templateUrl: './edit.component.html',
   styleUrl: './edit.component.css'
 })
