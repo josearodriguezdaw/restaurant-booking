922272d425b6fce46b4277438717fe52626fb9f0
Jose Antonio <profesorjoseantoniorodriguez@gmail.com>
2024-12-09 12:32:38 +0100

    Ej10: edit form validations

--- src/app/components/booking/booking-form/booking-form.component.ts
+++ src/app/components/booking/booking-form/booking-form.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-booking-form',
@@ -16,12 +16,12 @@ export class BookingFormComponent {
   constructor(formBuilder:FormBuilder){
     this.bookingForm = formBuilder.group({
       'id': ['', []],
-      'client': ['', []],
-      'phone': ['', []],
-      'email': ['', []],
-      'persons': ['', []],
-      'notes': ['', []],
-      'date': ['', []],
+      'client': ['', [Validators.required,Validators.minLength(0),Validators.maxLength(50)]],
+      'phone': ['', [Validators.required]],
+      'email': ['', [Validators.required]],
+      'persons': ['', [Validators.min(1),Validators.max(50)]],
+      'notes': ['', [Validators.maxLength(240)]],
+      'date': ['', [Validators.required]],
     });
   }
 }
