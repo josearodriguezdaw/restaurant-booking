e77398a3f7ecf0555d5bb5ef678fada736c93b3c
Jose Antonio <profesorjoseantoniorodriguez@gmail.com>
2024-12-09 11:54:44 +0100

    Ej2: config routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,4 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { BookingsComponent } from './pages/bookings/bookings.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { EditComponent } from './pages/edit/edit.component';
 
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:"home",component:HomeComponent},
+    {path:"bookings",component:BookingsComponent},
+    {path:"home",component:DashboardComponent},
+    {path:"edit/:id",component:EditComponent},
+    {path:"",redirectTo:"home",pathMatch:"full"},
+    {path:"**",redirectTo:"home",pathMatch:"full"}
+
+];
