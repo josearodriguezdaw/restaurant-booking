3948e020222e95a1ec42a0a2c4c91b6bd0d12f11
Jose Antonio <profesorjoseantoniorodriguez@gmail.com>
2024-12-09 12:24:34 +0100

    Ej9: edit reactive form configuration

--- src/app/components/booking/booking-form/booking-form.component.ts
+++ src/app/components/booking/booking-form/booking-form.component.ts
@@ -1,12 +1,27 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-booking-form',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule, ReactiveFormsModule],
   templateUrl: './booking-form.component.html',
   styleUrl: './booking-form.component.css'
 })
 export class BookingFormComponent {
 
+  bookingForm:FormGroup;
+
+  constructor(formBuilder:FormBuilder){
+    this.bookingForm = formBuilder.group({
+      'id': ['', []],
+      'client': ['', []],
+      'phone': ['', []],
+      'email': ['', []],
+      'persons': ['', []],
+      'notes': ['', []],
+      'date': ['', []],
+    });
+  }
 }
