431bf531c5e605514cee3e91235d22b5936aea91
Jose Antonio <profesorjoseantoniorodriguez@gmail.com>
2024-12-09 13:55:10 +0100

    Ej3: routing with childs

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,13 +2,17 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { BookingsComponent } from './pages/bookings/bookings.component';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProfileComponent } from './components/dashboard/profile/profile.component';
+import { StatsComponent } from './components/dashboard/stats/stats.component';
 import { EditComponent } from './pages/edit/edit.component';
 
 
 export const routes: Routes = [
     {path:"home",component:HomeComponent},
     {path:"bookings",component:BookingsComponent},
-    {path:"home",component:DashboardComponent},
+    {path:"dashboard",component:DashboardComponent,children: [
+        {path:"profile",component:ProfileComponent},
+        {path:"stats",component:StatsComponent}]},
     {path:"edit/:id",component:EditComponent},
     {path:"",redirectTo:"home",pathMatch:"full"},
     {path:"**",redirectTo:"home",pathMatch:"full"}
--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,11 +2,12 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { FooterComponent } from '../../components/footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [CommonModule, HeaderComponent,FooterComponent],
+  imports: [CommonModule, HeaderComponent,FooterComponent,RouterOutlet],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
