42b95c842c13a165edb3690c69b51d46e054e327
Jose Antonio <profesorjoseantoniorodriguez@gmail.com>
2024-12-10 11:53:57 +0100

    Add enviromernts data

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    projectId:"angular-proyect-a99c9",
+    appId:"1:49556754999:web:1315064af6e52b21bb25b2",
+    storageBucket:"angular-proyect-a99c9.firebasestorage.app",
+    apiKey:"AIzaSyA0fs6Vr02u5UwT5J0ov6Gj8sdjvx905nU",
+    authDomain:"angular-proyect-a99c9.firebaseapp.com",
+    messagingSenderId:"49556754999",
+    measurementId:"G-W7YYEKNQK7"
+};
